f677a2b2ea588eae97ef2a3cf4eea4aa10f10abb
KauriHero <kaurihero@masq.ai>
2024-05-09 15:33:07 +1200

    Update DApps.js
    adjusted to new draggable window format

--- featuredapp/DApps.js
+++ featuredapp/DApps.js
@@ -1,13 +1,16 @@
-// from MASQ-web3-store Public repo - modified 6 Mar 2024
+// from MASQ-web3-store Public repo - modified 9 May 2024
 // to be imported to path: app/renderer/components/Main/DApps/DApps.js
-// version 3.1
+// version 4.0
 
-import React from 'react';
+import React, {useState} from 'react';
 import Categories from './Categories';
 import AppStore from './AppStore';
-
+import classNames from 'classnames';
 import { categoriesData } from '../../../configs/dapps.js';
 
+import HeaderIcon from '../../Main/Header/HeaderIcon';
+import closeIcon from '../../../assets/images/close.svg';
+import maximizeIcon from '../../../assets/icons/maximize-icon.svg';
 import feature1 from '../../../assets/images/dapps/dapp_feat1_masq_banner.png';
 import feature2 from '../../../assets/images/dapps/dapp_feat2_crucible-resized.png';
 import feature3 from '../../../assets/images/dapps/dapp_feat3_FlufWorld.png';
@@ -31,11 +34,12 @@ const featureAppData = [
 ];
 
 const DApps = (props) => {
-    const {size} = props
+    const { size, onClick, appElement, onMinimizeHandle, setDappPosition, DappInitialPosition, setSize, setDappMaximized,dappMaximized} = props
     const [selectItem, setSelectItem] = React.useState({});
     const [categories, setCategories] = React.useState([]);
     const [searchQuery, setSearchQuery] = React.useState('');
     const [appListData, setAppListData] = React.useState([]);
+    
     const storeApps = window.api.getStoreValue('launcher.apps')[window.api.getStoreValue('workspace')]
     const presenceArray = appListData.map(itemB => storeApps.some(itemA => itemA.link.includes(itemB.name.toLowerCase())));
     const searchParams = ['name'];
@@ -59,7 +63,6 @@ const DApps = (props) => {
             // dispatch(toggleDock(false))
         }
     };
-
 
     const handleSetQuery = query => {
         setSearchQuery(query);
@@ -89,28 +92,82 @@ const DApps = (props) => {
         setAppListData(appList);
     }, [searchQuery, selectItem]);
 
+    const handleMaximizeWindow = () => {
+        setDappMaximized(prev => !prev);
+        if (!dappMaximized) {
+            // have a width of 97%
+            const width = 0.97 * window.innerWidth;
+            // have height 0f 82%
+            const height = 0.82 * window.innerHeight;
+            const centerX = (window.innerWidth - width) / 2 + 44.25;
+            const centerY = (window.innerHeight - height) / 2 - 51.15;
+            setDappPosition({ x: centerX, y: centerY });
+            setSize({ width: width, height: height });
+        } else {
+            setDappPosition({
+                x: window.innerWidth / 2 - 500,
+                y: window.innerHeight / 2 - 350,
+            });
+            setSize({ width: 1150, height: 600 });
+        }
+    };
     return (
-        <div className='dApps'>
-        {/* <div className="dApps__gradient" /> */}
-        <Categories
-            data={categories}
-            activeItem={selectItem}
-            setActiveItem={item => setSelectItem(item)}
-            setQuery={handleSetQuery}
-            searchQuery={searchQuery}
-        />
-        {selectItem.icon && categories.length > 0 && (
-            <AppStore
-                size={size}
-                category={selectItem}
-                featureAppData={featureAppData}
-                appListData={appListData}
-                isPresent={presenceArray}
-                onClickFavorite={handleClickFavorite}
+        <div className='dApps' onClick={onClick}>
+            <div
+                    className={classNames('handle')}
+                    
+                >
+                    <div className='btnWrap'>
+                        <div
+                            className='maximize'
+                            onClick={handleMaximizeWindow}
+                        >
+                            <img src={maximizeIcon} />
+                        </div>
+                        <HeaderIcon
+                            image={closeIcon}
+                            classname={'buttons close'}
+                            onClick={() => {
+                                onMinimizeHandle('dapp' )
+                                setDappPosition(DappInitialPosition)
+                                setSize({
+                                    width: 1170,
+                                    height: 600,
+                                })
+                                setDappMaximized(false)
+                            }}
+                        />
+
+                        {/* <HeaderIcon
+                        image={maximizeIcon}
+                        classname={'buttons maximize'}
+                        onClick={handleMaximizeWindow}
+                    /> */}
+                    </div>
+                    <div className='handle-title'>
+                        <img src={appElement.item.icon.url}></img>
+                        <span>{appElement.item.name}</span>
+                    </div>
+                </div>
+            <Categories
+                data={categories}
+                activeItem={selectItem}
+                setActiveItem={item => setSelectItem(item)}
+                setQuery={handleSetQuery}
                 searchQuery={searchQuery}
             />
-        )}
-    </div>
+            {selectItem.icon && categories.length > 0 && (
+                <AppStore
+                    size={size}
+                    category={selectItem}
+                    featureAppData={featureAppData}
+                    appListData={appListData}
+                    isPresent={presenceArray}
+                    onClickFavorite={handleClickFavorite}
+                    searchQuery={searchQuery}
+                />
+            )}
+        </div>
     );
 };
 
